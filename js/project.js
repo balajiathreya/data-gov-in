89146008fef2127dd4ce16559e9590b01a15daec
balajiathreya <athreya86@gmail.com>
2014-09-06 16:25:58 -0400

    initial commit - skeleton

--- js/project.js
+++ js/project.js
@@ -1,46 +1,30 @@
-angular.module('tweetMapModule', ['google-maps'])
-.controller('TweetMapController', ['$scope','$http', function ($scope,$http) {
+
+var dataGovINApp = angular.module('dataGovINApp', ['ngRoute','google-maps']);
+
+// configure our routes
+dataGovINApp.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : 'pages/home.html'
+		})
+		// TN urban population
+		.when('/tn_ulb_pop', {
+			templateUrl : 'pages/tn_ulb_pop.html',
+			controller  : 'tnULBPopController'
+		});
+	});
+
+// define controllers for each page
+dataGovINApp.controller('tnULBPopController', ['$scope','$http', function ($scope,$http) {
+  
   // Enable the new Google Maps visuals until it gets enabled by default.
   // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
   google.maps.visualRefresh = true;
   
-  // defaults to a locale which is global
-  DEFAULT_WOEID = 1;
 
-  // default latitude and longitude values. kinda at the center of the global
-  // in the north pacific ocean
-  $scope.lat = 45.521071299999996;
-  $scope.lon = -122.88757439999998;
-  $scope.trending = '';
-  $scope.selectedLocale = '';
-
-  var setGeoLocation = function(){
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          function(position){
-            $scope.lat = position.coords.latitude;
-            $scope.lon = position.coords.longitude;
-          },
-          function( error ){
-            console.log( "Something went wrong: ", error );
-          },
-          {}
-        );
-    }    
-  };
-
-  var setTrendingData = function(woeid){
-    url = "http://twitterhelper.balajiathreya.com/gettrending?woeid="+woeid;
-    var responsePromise = $http.get(url);
-    console.log('woeid '+woeid);
-    responsePromise.success(function(data, status, headers, config) {      
-      $scope.trending = data;
-    });
-
-    responsePromise.error(function(data, status, headers, config) {
-      $scope.trending = data;
-    });  
-  };
+  // default latitude and longitude values. set to Salem which is kinda at the center of Tamil Nadu
+  $scope.lat = 42.51954;
+  $scope.lon = -70.8967155;
 
   $scope.map = {
     center: {        
@@ -48,34 +32,8 @@ angular.module('tweetMapModule', ['google-maps'])
         longitude: $scope.lon
     },
     options: { draggable: true },
-    zoom: 3,
-    events : {
-      'click': function (mapModel, eventName, originalEventArgs) {
-        var e = originalEventArgs[0];
-        var lat = e.latLng.lat(),
-        lon = e.latLng.lng();
-        url = "http://twitterhelper.balajiathreya.com/getWOEID?lat="+lat+'&lon='+lon
-        responsePromise = $http.get(url);
+    zoom: 6,
+    events : {}
+ };
 
-        responsePromise.success(function(data, status, headers, config) {
-	  $scope.trending = data[0];
-          $scope.title = 'Trending Topics near ' + data[0].name;
-          woeid = data[0].woeid;          
-          setTrendingData(woeid);
-        });
-
-        responsePromise.error(function(data, status, headers, config) {
-          $scope.trending = data;
-          $scope.title = "Couldn't get locale. Displaying trending topics worldwide";
-          setTrendingData(DEFAULT_WOEID);
-        });
-        
-        // make service call here
-        //scope apply required because this event handler is outside of the angular domain
-        $scope.$apply();
-      }    
-    }
-  };
-
-  setGeoLocation();
 }]);
